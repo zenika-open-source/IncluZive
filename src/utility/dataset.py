3111d74ad7f60b5e0386fd5e8d3f7a2990fa4245
AmalBe <amal.bedoui@zenika.com>
2021-05-18 16:49:41 +0200

    Adding --seed argument

--- src/utility/dataset.py
+++ src/utility/dataset.py
@@ -19,12 +19,15 @@ def annotation_to_bio_tags(text: str, annotations: List[Tuple[str, str]]) -> Lis
     return list(token_dict.items())
 
 
-def create_train_test_dataset_from_workbooks(workbooks: List[str], train_file: str, test_file: str, split: float = 0.2):
+def create_train_test_dataset_from_workbooks(
+    workbooks: List[str], train_file: str, test_file: str, split: float = 0.2, seed: int = 3
+):
     """
     Write train/test files from workbooks in CoNLL column-format
     """
     worksheets = (load_workbook(file).active for file in workbooks)
     annotated_texts = [annotation for sheet in worksheets for annotation in load_annotations(sheet)]
+    random.seed(seed)
     random.shuffle(annotated_texts)
     split_idx = int(split * len(annotated_texts))
 
@@ -54,9 +57,14 @@ if __name__ == "__main__":
     parser.add_argument("src", help="Annotated workbooks")
     parser.add_argument("dest", help="Destination directory")
     parser.add_argument("--split", help="Split data", type=float)
+    parser.add_argument("--seed", help="seed value", type=int)
     args = parser.parse_args()
 
     workbook_files = glob.glob(os.path.join(args.src, "*.xlsx")) if os.path.isdir(args.src) else [args.src]
     create_train_test_dataset_from_workbooks(
-        workbook_files, train_file=args.dest + "/train.txt", test_file=args.dest + "/test.txt", split=args.split
+        workbook_files,
+        train_file=args.dest + "/train.txt",
+        test_file=args.dest + "/test.txt",
+        split=args.split,
+        seed=args.seed,
     )
