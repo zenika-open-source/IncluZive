5680a791ffa495f01da7e19b069cbab199880671
Amal Bedoui <46492266+AmalBe@users.noreply.github.com>
2021-05-18 16:56:03 +0200

    Data split arguments  (#37)
    * Data split arguments updat
    * Adding --seed argument

--- src/utility/dataset.py
+++ src/utility/dataset.py
@@ -19,12 +19,15 @@ def annotation_to_bio_tags(text: str, annotations: List[Tuple[str, str]]) -> Lis
     return list(token_dict.items())
 
 
-def create_train_test_dataset_from_workbooks(workbooks: List[str], train_file: str, test_file: str, split: float = 0.2):
+def create_train_test_dataset_from_workbooks(
+    workbooks: List[str], train_file: str, test_file: str, split: float = 0.2, seed: int = 3
+):
     """
     Write train/test files from workbooks in CoNLL column-format
     """
     worksheets = (load_workbook(file).active for file in workbooks)
     annotated_texts = [annotation for sheet in worksheets for annotation in load_annotations(sheet)]
+    random.seed(seed)
     random.shuffle(annotated_texts)
     split_idx = int(split * len(annotated_texts))
 
@@ -45,14 +48,23 @@ def create_train_test_dataset_from_workbooks(workbooks: List[str], train_file: s
         file.writelines(lines)
 
     with open(test_file, "w") as file:
-        lines = (f"{token} {bio_tag}" if token else "\n" for token, bio_tag in test_bio_tags)
+        lines = (f"{token} {bio_tag}\n" if token else "\n" for token, bio_tag in test_bio_tags)
         file.writelines(lines)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("src", help="Annotated workbooks")
+    parser.add_argument("dest", help="Destination directory")
+    parser.add_argument("--split", help="Split data", type=float)
+    parser.add_argument("--seed", help="seed value", type=int)
     args = parser.parse_args()
 
     workbook_files = glob.glob(os.path.join(args.src, "*.xlsx")) if os.path.isdir(args.src) else [args.src]
-    create_train_test_dataset_from_workbooks(workbook_files, train_file="train.txt", test_file="test.txt")
+    create_train_test_dataset_from_workbooks(
+        workbook_files,
+        train_file=args.dest + "/train.txt",
+        test_file=args.dest + "/test.txt",
+        split=args.split,
+        seed=args.seed,
+    )
--- tests/core/test_workbook.py
+++ tests/core/test_workbook.py
@@ -9,16 +9,16 @@ from core.workbook import load_annotations
     [
         (
             """
-Text	page	x1	y1	x2	y2	Entity	Label
-06/07/2018 zenika-resume 	0	26,4531002044678	14,7578125	358,363220214844	23,6953125	06/07/2018	DATE
+Text	Entity	Label
+06/07/2018 zenika-resume 	06/07/2018	DATE
 """,
             [["06/07/2018 zenika-resume ", [("06/07/2018", "DATE")]]],
         ),
         (
             """
-Text	page	x1	y1	x2	y2	Entity	Label
-06/07/2018 zenika-resume 	0	26,4531002044678	14,7578125	358,363220214844	23,6953125	06/07/2018	DATE
-Velay S 6 ans d'expérience 	0	28,0065994262695	76,0113220214844	565,976501464844	111,218208312988	Velay S	PER
+Text	Entity	Label
+06/07/2018 zenika-resume 	06/07/2018	DATE
+Velay S 6 ans d'expérience 	Velay S	PER
 """,
             [
                 ["06/07/2018 zenika-resume ", [("06/07/2018", "DATE")]],
@@ -27,17 +27,17 @@ Velay S 6 ans d'expérience 	0	28,0065994262695	76,0113220214844	565,97650146484
         ),
         (
             """
-Text	page	x1	y1	x2	y2	Entity	Label
-06/07/2018 zenika-resume 	0	26,4531002044678	14,7578125	358,363220214844	23,6953125	06/07/2018	DATE
-06/07/2018 zenika-resume 	1	26,453125	14,7578125	358,363464355469	23,6953125	06/07/2018	DATE
+Text	Entity	Label
+06/07/2018 zenika-resume 	06/07/2018	DATE
+06/07/2018 zenika-resume 	06/07/2018	DATE
 """,
             [["06/07/2018 zenika-resume ", [("06/07/2018", "DATE")]]],
         ),
         (
             """
-Text	page	x1	y1	x2	y2	Entity	Label
-Velay S 6 ans d'expérience 	0	28,0065994262695	76,0113220214844	565,976501464844	111,218208312988	Velay S	PER
-Velay S 6 ans d'expérience 	0	28,0065994262695	76,0113220214844	565,976501464844	111,218208312988	6 ans	PERIODE
+Text	Entity	Label
+Velay S 6 ans d'expérience 	Velay S	PER
+Velay S 6 ans d'expérience 	6 ans	PERIODE
 """,
             [
                 ["Velay S 6 ans d'expérience ", [("Velay S", "PER"), ("6 ans", "PERIODE")]],
@@ -45,7 +45,7 @@ Velay S 6 ans d'expérience 	0	28,0065994262695	76,0113220214844	565,97650146484
         ),
         (
             """
-Text	page	x1	y1	x2	y2	Entity	Label
+Text	Entity	Label
 Velay S 6 ans d'expérience\t\t\t\t\t\t\t
 """,
             [],
